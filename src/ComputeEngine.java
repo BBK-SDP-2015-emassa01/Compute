210ae138c8493e7575d15dce2540a2e664079b97
Esha Massand <ubemas001@mail.bbk.ac.uk>
2014-03-17 20:34:18 +0000

    Java Tutorial

---
+++ src/ComputeEngine.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import compute.Compute;
+import compute.Task;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Esha
+ */
+public class ComputeEngine extends UnicastRemoteObject implements Compute {
+    
+    public ComputeEngine() throws RemoteException{
+        super();
+    }
+    
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
+
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Compute";
+            Compute engine = new ComputeEngine();
+            Compute stub =
+                (Compute) UnicastRemoteObject.exportObject(engine, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
+        }
+    }
+    
+}
